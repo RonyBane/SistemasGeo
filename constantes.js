07146ef5660372bb316f6e48e0a2df275d62f608
BarrientosRonaldo <42949656+RonyBane@users.noreply.github.com>
2020-03-27 19:19:13 -0600

    Create constantes.js

---
+++ constantes.js
@@ -0,0 +1,4 @@
+
+    const productoslista = document.querySelector("#lista");
+    const formulario = document.querySelector("#forma");
+    const formularioeditar = document.querySelector("#formaeditar");
